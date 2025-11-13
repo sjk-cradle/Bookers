ddae7355f144d1fde6b647b2652962f99a552ecc
Ubuntu <ubuntu@ip-172-31-44-177.ap-northeast-1.compute.internal>
2025-11-13 16:02:55 +0000

    [Save] destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   get '/top' => 'homes#top'
-  resources :books
+  #get 'books/new'
+  get 'books/index'
+  post 'books' => 'books#create'
+  get 'books/:id' => 'books#show', as: 'book'
+  get 'books/:id/:edit' => 'books#edit', as: 'edit_book'
+  patch 'books/:id' => 'books#update', as: 'update_book'
+  delete 'books/:id' => 'books#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
