d1184a9354606854a8bdd5bdf69a38ff27d8869e
Ubuntu <ubuntu@ip-172-31-44-177.ap-northeast-1.compute.internal>
2025-11-18 07:33:08 +0000

    [Finish] broad outline

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,13 +11,14 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-      flash[:notice] = "投稿に成功しました。"
+      flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
-      flash.now[:alert] = "投稿に失敗しました。"
-      render :new
+      
+      @books = Book.all
+      render :index
+    end
   end
-end
 
   def show
     @book = Book.find(params[:id])
